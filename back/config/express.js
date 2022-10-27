e92b05240e50cac00edcca7032178ce976e09d20
doonee <doonee@outlook.com>
2022-10-27 11:17:35 +0900

    express static url

--- back/config/express.js
+++ back/config/express.js
@@ -4,22 +4,22 @@ const methodOverride = require("method-override");
 var cors = require("cors");
 
 module.exports = function () {
-  const app = express();
+    const app = express();
 
-  app.use(compression());
+    app.use(compression());
 
-  app.use(express.json());
+    app.use(express.json());
 
-  app.use(express.urlencoded({ extended: true }));
+    app.use(express.urlencoded({ extended: true }));
 
-  app.use(methodOverride());
+    app.use(methodOverride());
 
-  app.use(cors());
-  app.use(express.static("/home/ubuntu/food-map/front"));
+    app.use(cors());
+    //app.use(express.static("/home/ubuntu/food-map/front"));
 
-  // app.use(express.static(process.cwd() + '/public'));
+    // app.use(express.static(process.cwd() + '/public'));
 
-  require("../src/routes/indexRoute")(app);
+    require("../src/routes/indexRoute")(app);
 
-  return app;
+    return app;
 };
